59dfac2e54115e9c61a42b9db84638d5f84182db
Slesha <sleshamishra4@gmail.com>
2020-08-17 12:29:48 +1000

    nav_bar

--- room_booking/src/App.js
+++ room_booking/src/App.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import './App.css';
 
-import Home from './pages/Home';
-import Rooms from './pages/Rooms';
-import SingleRoom from './pages/SingleRoom';
-import Error from './pages/Error';
+import Home from './pages/Home'
+import Rooms from './pages/Rooms'
+import SingleRoom from './pages/SingleRoom'
+import Error from './pages/Error'
 
-import {Route, Switch} from 'react-router-dom';
+import {Route, Switch} from 'react-router-dom'
+
+import Navbar from  './components/Navbar'
 
 function App() {
   return (
-    <div className="App">
-      <Route exact path= '/' component={Home}/>
-      <Route exact path= '/rooms/' component={Rooms}/>
-      <Route exact path= '/single-room' component={SingleRoom}/>
+    <div>
+      <Navbar />
+      <Switch>
+        <Route exact path= '/' component={Home}/>
+        <Route exact path= '/rooms/' component={Rooms}/>
+        <Route exact path= '/rooms/:meow' component={SingleRoom}/>
+        <Route component={Error}/>
+      </Switch>
     </div>
   );
 }
